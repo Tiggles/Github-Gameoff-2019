58008e08c5ee2068c561bf343dc8e393ccd5d1c2
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-11-03 18:59:13 +0100

    Add tilemap

--- GGGG/Player.gd
+++ GGGG/Player.gd
@@ -8,9 +8,12 @@ var velocity: Vector2
 
 var acceleration: Vector2
 
-export var max_movement_speed: int = 1000
+export var max_movement_speed: int = 600
 # FIXME: find better name
-export var movement_acceleration: int = 200
+export var movement_acceleration: int = 60
+
+export var max_fall_speed: int = 300
+export var fall_acceleration: int = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,16 +25,20 @@ func _process(delta: float) -> void:
 	
 func get_input(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = max(min(velocity.x + movement_acceleration * delta, max_movement_speed), -max_movement_speed)
+		velocity.x = max(min(velocity.x + movement_acceleration, max_movement_speed), -max_movement_speed)
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = min(max(velocity.x - movement_acceleration * delta, -max_movement_speed), max_movement_speed)
+		velocity.x = min(max(velocity.x - movement_acceleration, -max_movement_speed), max_movement_speed)
 	if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
 		if velocity.x > 0:
-			velocity.x = max(velocity.x - movement_acceleration * delta, 0)
+			velocity.x = max(velocity.x - movement_acceleration, 0)
 		else:
-			velocity.x = min(velocity.x + movement_acceleration * delta, 0)
-
+			velocity.x = min(velocity.x + movement_acceleration, 0)
+			
+	velocity.y = min(velocity.y + fall_acceleration, max_fall_speed)
+	
+	if is_on_floor(): print("ya")
+	
 func _physics_process(delta: float):
 	get_input(delta)
-	var val: Vector2 = move_and_slide(velocity)
+	move_and_slide(velocity, Vector2(0, -1))
 
