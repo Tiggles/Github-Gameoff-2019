2c2d78b9b46680866531eae51fca386226ee177c
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-11-03 13:36:00 +0100

    Better movement

--- GGGG/Player.gd
+++ GGGG/Player.gd
@@ -4,27 +4,34 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-export var movement_speed: int = 800
-export var max_movement_speed: int = 1000
 var velocity: Vector2
+
+var acceleration: Vector2
+
+export var max_movement_speed: int = 1000
+# FIXME: find better name
+export var movement_acceleration: int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	acceleration = Vector2(0, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func get_input() -> void:
-	velocity = Vector2()
+func get_input(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x = max(min(velocity.x + movement_acceleration * delta, max_movement_speed), -max_movement_speed)
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	velocity = velocity.normalized() * movement_speed;
+		velocity.x = min(max(velocity.x - movement_acceleration * delta, -max_movement_speed), max_movement_speed)
+	if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+		if velocity.x > 0:
+			velocity.x = max(velocity.x - movement_acceleration * delta, 0)
+		else:
+			velocity.x = min(velocity.x + movement_acceleration * delta, 0)
 
 func _physics_process(delta: float):
-	get_input()
-	move_and_slide(velocity)
+	get_input(delta)
+	var val: Vector2 = move_and_slide(velocity)
 
