6c7bc4ee390ad2e1db1368df97c870b54d2eed52
Jonas Malte Hinchely <jonas@hinchely.dk>
2019-11-04 18:47:29 +0100

    Walljump

--- GGGG/Player.gd
+++ GGGG/Player.gd
@@ -13,10 +13,12 @@ export var max_movement_speed: int = 600
 export var movement_acceleration: int = 60
 export var max_fall_speed: int = 500
 export var fall_acceleration: int = 60
+var moving_left = false
 
 # Jumping
 var can_jump: bool = false
 export var jump_velocity: int = 1200
+var can_wall_jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -31,6 +33,8 @@ func get_input(delta: float) -> void:
 	var is_touching = self.is_on_floor() or self.is_on_wall()
 	
 	var curr_mvmt_accl = movement_acceleration if is_touching else movement_acceleration / 2
+	can_wall_jump = self.is_on_wall() and !self.is_on_floor()
+	
 	
 	if is_on_ceiling():
 		self.velocity.y = 1
@@ -48,7 +52,14 @@ func get_input(delta: float) -> void:
 	velocity.y = min(velocity.y + fall_acceleration, max_fall_speed)
 	
 	if Input.is_action_pressed("ui_up"):
-		if can_jump:
+		if can_wall_jump and velocity.x != 0:
+			if moving_left:
+				velocity.y = -jump_velocity * 0.8
+				velocity.x = 900
+			else:
+				velocity.y = -jump_velocity * 0.8
+				velocity.x = -900
+		elif can_jump:
 			velocity.y = -jump_velocity
 	
 	# standard jumping or wall jump
@@ -56,6 +67,8 @@ func get_input(delta: float) -> void:
 		can_jump = true
 	else:
 		can_jump = false
+		
+	moving_left = velocity.x < 0
 	
 func _physics_process(delta: float):
 	get_input(delta)
