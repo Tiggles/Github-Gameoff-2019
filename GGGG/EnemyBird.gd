71b85956586a1e2f8c9a6953fa50b4d6e04d7c79
Brian Alberg <brian@alberg.org>
2019-11-23 00:20:47 +0100

    Added follow delay to enemybird

--- GGGG/EnemyBird.gd
+++ GGGG/EnemyBird.gd
@@ -8,27 +8,34 @@ enum { MOVING_DOWN = -1, MOVING_UP = 1}
 export var default_moving_direction_x = MOVING_RIGHT
 var moving_direction_x = default_moving_direction_x
 var moving_direction_y = MOVING_DOWN
+var time_start = 0
+var time_now = 0
+
 
 var velocity = Vector2(0, 0)
 
 func _ready():
-	pass
+	var time_start = OS.get_unix_time()
 
 func _physics_process(delta: float) -> void:
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
 	var collision = move_and_slide(velocity * delta)
 
-	var player = get_tree().get_root().get_node("Main").get_node("Player")
-	if (player.position.x < self.position.x):
-		$Sprite.flip_h = false
-		self.moving_direction_x = MOVING_LEFT
-	else:
-		$Sprite.flip_h = true
-		self.moving_direction_x = MOVING_RIGHT
+	if (elapsed % 2 == 0):
+		var player = get_tree().get_root().get_node("Main").get_node("Player")
+		if (player.position.x < self.position.x):
+			$Sprite.flip_h = false
+			self.moving_direction_x = MOVING_LEFT
+		else:
+			$Sprite.flip_h = true
+			self.moving_direction_x = MOVING_RIGHT
+	
+		if (player.position.y < self.position.y):
+			self.moving_direction_y = MOVING_DOWN
+		else:
+			self.moving_direction_y = MOVING_UP
 
-	if (player.position.y < self.position.y):
-		self.moving_direction_y = MOVING_DOWN
-	else:
-		self.moving_direction_y = MOVING_UP
-
-	self.velocity.x = speed * speed_factor * moving_direction_x
-	self.velocity.y = speed * speed_factor * moving_direction_y
+	
+		self.velocity.x = speed * speed_factor * moving_direction_x
+		self.velocity.y = speed * speed_factor * moving_direction_y
